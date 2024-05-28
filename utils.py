59797e18197d31c8642dc0e3b5bdfb59e7399f12
Antonio Spagnuolo <101936197+aspagnuolo@users.noreply.github.com>
2024-05-28 09:13:14 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -208,7 +208,7 @@ def get_top_bigrams(text, n=None):
     list: A list of bigrams.
     """
     bigrams = ngrams(text, 2)
-    return [bigram for bigram in bigrams]
+    return [' '.join(bigram) for bigram in bigrams]
 
 def extract_bigrams(df):
     """
